8ddbf5d79e4359f580e3f174e2e69da652406d7c
Satyam Karki <74590592+satyam-karki@users.noreply.github.com>
2024-03-27 19:02:14 +0545

    Update Program.cs

--- Calculator_App_Broadway/Program.cs
+++ Calculator_App_Broadway/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
 Console.WriteLine("Welcome to Broadway Calculaltor");
+
+
 Console.WriteLine("Please provide the first iput");
+int firstInput = int.Parse(Console.ReadLine());
+
+
 Console.WriteLine("Please provide the second input");
+int secondInput = int.Parse(Console.ReadLine());
+
+int sum = firstInput + secondInput;
+Console.WriteLine("Sum is sum");
