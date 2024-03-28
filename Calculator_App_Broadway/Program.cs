7d90ce987dcd3e7d56096dea25903de1c1d8eff6
Satyam Karki <74590592+satyam-karki@users.noreply.github.com>
2024-03-28 16:08:40 +0545

    Update Program.cs

--- Calculator_App_Broadway/Program.cs
+++ Calculator_App_Broadway/Program.cs
@@ -1,18 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Numerics;
-
-Console.WriteLine("Welcome to Broadway Calculaltor");
+//Console.WriteLine("Welcome to Broadway Calculaltor");
 
 
-Console.WriteLine("Please provide the first iput");
-int firstInput = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please provide the first iput");
+//int firstInput = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("Please provide the second input");
-int secondInput = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please provide the second input");
+//int secondInput = int.Parse(Console.ReadLine());
 
-int sum = firstInput + secondInput;
-Console.WriteLine(($"The result is : {sum}"));
+//int sum = firstInput + secondInput;
+//Console.WriteLine(($"The result is : {sum}"));
 
 
 
@@ -20,3 +18,22 @@ Console.WriteLine(($"The result is : {sum}"));
 //substraction
 //,ultiplication
 //division
+
+Console.WriteLine("Welconme to Broadway Calculator");
+
+Console.WriteLine("Please provide the first input");
+int firstInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please provide the second input");
+int secondInput = int.Parse(Console.ReadLine());
+
+int Add = firstInput + secondInput;
+int Sub = firstInput - secondInput;
+int Multiply = firstInput * secondInput;
+int Divide = firstInput / secondInput;  
+
+
+Console.WriteLine(($"The result is : {Add}"));
+Console.WriteLine(($"The result is : {Sub}"));
+Console.WriteLine(($"The result is : {Multiply}"));
+Console.WriteLine(($"The result is : {Divide}"));
